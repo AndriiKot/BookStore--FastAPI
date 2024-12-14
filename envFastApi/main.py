5cfd5f2032c9adb163734ebb05350ea460a45878
AndriiKot <AndriiKot@users.noreply.github.com>
2024-12-14 19:38:04 +0100

    'FastAPI' 'easy' 'Bookstore' ! add runnig parameter '--port' and defult '8000'  !

--- envFastApi/main.py
+++ envFastApi/main.py
@@ -1,28 +1,33 @@
 ﻿from fastapi import FastAPI
 import uvicorn
 import os
+import argparse
 
 app = FastAPI()
 
 books = [
-	{
+    {
         "id": 1,
         "title": "Python Programming",
-		"author": "John Doe",
-		"price": 19.99
-	},
+        "author": "John Doe",
+        "price": 19.99
+    },
     {
-		"id": 2,
+        "id": 2,
         "title": "FastAPI",
-		"author": "John Doe",
-		"price": 29.99
-	}
+        "author": "John Doe",
+        "price": 29.99
+    }
 ]
 
 @app.get("/books")
 async def get_books():
-	return books
+    return books
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the FastAPI app.")
+    parser.add_argument("--port", type=int, default=8000, help="Port to run the app on.")
+    args = parser.parse_args()
+
     module_name = os.path.splitext(os.path.basename(__file__))[0]
-    uvicorn.run(module_name + ":app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(module_name + ":app", host="127.0.0.1", port=args.port, reload=True)
